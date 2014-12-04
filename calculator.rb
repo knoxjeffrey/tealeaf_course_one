3de64ee64a38194f10f88ed136941fed61fe984a
Jeff Knox <knoxjeffrey@outlook.com>
2014-12-04 01:51:59 +0000

    updated calculator

--- calculator.rb
+++ calculator.rb
@@ -10,6 +10,14 @@ class String
         print_string "Enter a valid operator. Please try again"
       end
   end
+  
+  def valid_decision?
+    if ['Y', 'N'].include?(self)
+      true
+    else
+      print_string "You must enter Y or N. Please try again"
+    end
+  end
 end
 
 def print_string(text)
@@ -20,30 +28,44 @@ def chomp_it
   gets.chomp
 end
 
-begin
-  print_string "Please enter your first number"
-  num1 = chomp_it
-end while !num1.numeric?
+def calculator
+  begin
+    print_string "Please enter your first number"
+    num1 = chomp_it
+  end while !num1.numeric?
   
-begin
-print_string "Please enter your second number"
-num2 = chomp_it
-end while !num2.numeric?
+  begin
+  print_string "Please enter your second number"
+  num2 = chomp_it
+  end while !num2.numeric?
 
-begin
-print_string "Select operator: 1.Add 2.Subtract 3.Multiply 4.Divide"
-operator = chomp_it
-end while !operator.valid_operator?
+  begin
+  print_string "Select operator: 1.Add 2.Subtract 3.Multiply 4.Divide"
+  operator = chomp_it
+  end while !operator.valid_operator?
 
-case operator
-when '1' || 'Add'
-  result = num1.to_f + num2.to_f
-when '2' || 'Subtract'
-  result = num1.to_f - num2.to_f
-when '3' || 'Multiply'
-  result = num1.to_f * num2.to_f
-when '4' || 'Divide'
-  result = num1.to_f / num2.to_f
+  case operator
+  when '1', 'Add'
+    result = num1.to_f + num2.to_f
+  when '2', 'Subtract'
+    result = num1.to_f - num2.to_f
+  when '3', 'Multiply'
+    result = num1.to_f * num2.to_f
+  when '4', 'Divide'
+    result = num1.to_f / num2.to_f
+  end
+
+  print_string "The result is #{result}"
 end
 
-print_string "The result is #{result}"
+begin
+  calculator
+  
+  begin
+    print_string "Would you like to perform another calculation: Y or N"
+    decision = chomp_it
+  end while !decision.valid_decision?
+  
+end while decision == 'Y'
+  
+  
