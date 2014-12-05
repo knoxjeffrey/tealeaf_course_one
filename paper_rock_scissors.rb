4b815c22faec5990d449fae91db71b98d229070c
Jeff Knox <knoxjeffrey@outlook.com>
2014-12-05 00:44:25 +0000

    code tidied up

--- paper_rock_scissors.rb
+++ paper_rock_scissors.rb
@@ -1,6 +1,6 @@
 class String
   def valid_choice?
-      if ['P', 'R', 'S'].include?(self)
+      if ['p', 'r', 's'].include?(self)
         true
       else
         print_string "Enter a valid choice. Please try again"
@@ -8,7 +8,7 @@ class String
   end
   
   def valid_decision?
-    if ['Y', 'N'].include?(self)
+    if ['y', 'n'].include?(self)
       true
     else
       print_string "You must enter Y or N. Please try again"
@@ -24,36 +24,31 @@ def chomp_it
   gets.chomp
 end
 
-def computer_selection
-  computer_choices = ['Paper', 'Rock', 'Scissors']
-  computer_choices.sample
-end
-
 def outcome(player, computer)
-  winning_combo = { 'Paper' => 'Rock', 'Rock' => 'Scissors', 'Scissors' => 'Paper' }
   case
-    when winning_combo[player] == computer then "You win!"
-    when winning_combo[computer] == player then "You lose"
+    when WINNING_COMBO[player] == computer then "You win!"
+    when WINNING_COMBO[computer] == player then "You lose"
     else "It's a tie"
   end
 end
 
 def paper_rock_scissor
-  player_choices = { P: 'Paper', R: 'Rock', S: 'Scissors' }
-
   print_string "Play Paper Rock Scissors!"
 
   begin
   print_string "Choose one: P/R/S"
-  game = chomp_it
-  end while !game.valid_choice?
+  choice = chomp_it
+  end while !choice.downcase.valid_choice?
 
-  computer_pick = computer_selection
-  player_pick = player_choices[game.to_sym]
+  computer_pick = CHOICES.values.sample
+  player_pick = CHOICES[choice.downcase.to_sym]
   print_string "You picked #{player_pick} and computer picked #{computer_pick}"
 
   puts outcome(player_pick, computer_pick)
 end
+
+CHOICES = { p: 'Paper', r: 'Rock', s: 'Scissors' }
+WINNING_COMBO = { 'Paper' => 'Rock', 'Rock' => 'Scissors', 'Scissors' => 'Paper' }
 
 begin
   paper_rock_scissor
@@ -61,6 +56,6 @@ begin
   begin
     print_string "Would you like to play again?: Y or N"
     decision = chomp_it
-  end while !decision.valid_decision?
+  end while !decision.downcase.valid_decision?
   
-end while decision == 'Y'
+end while decision.downcase == 'y'
