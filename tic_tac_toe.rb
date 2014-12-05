64b13147afcefa9669508d03b0b092c80dde5bac
Jeff Knox <knoxjeffrey@outlook.com>
2014-12-05 16:40:49 +0000

    redone without using global variable

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,27 +1,27 @@
-def the_empty_positions
-  $game_inputs.select { |k,v| v == ' ' }.keys
+def the_empty_positions(current_game_inputs)
+  current_game_inputs.select { |k,v| v == ' ' }.keys
 end
 
-def check_valid_entry(number_selected)
-  if !the_empty_positions.include?(number_selected.to_i)
+def check_valid_entry(number_selected, current_game_inputs)
+  if !the_empty_positions(current_game_inputs).include?(number_selected.to_i)
     puts "Invalid choice or that position is already taken.  Please try again."
   else
     true
   end
 end
 
-def display_grid
+def display_grid(game_inputs)
   puts "
      |     |
-  #{$game_inputs[1]}  |  #{$game_inputs[2]}  |  #{$game_inputs[3]}
+  #{game_inputs[1]}  |  #{game_inputs[2]}  |  #{game_inputs[3]}
      |     |
 -----+-----+-----
      |     |
-  #{$game_inputs[4]}  |  #{$game_inputs[5]}  |  #{$game_inputs[6]}
+  #{game_inputs[4]}  |  #{game_inputs[5]}  |  #{game_inputs[6]}
      |     |
 -----+-----+-----
      |     |
-  #{$game_inputs[7]}  |  #{$game_inputs[8]}  |  #{$game_inputs[9]}
+  #{game_inputs[7]}  |  #{game_inputs[8]}  |  #{game_inputs[9]}
      |     |     "
 end
 
@@ -36,46 +36,46 @@ def a_winning_combo?(player_or_computer_array)
   answer
 end
 
-def game_over?
-  if a_winning_combo?($player_moves_made)
+def game_over?(current_game_inputs, players_moves, computer_moves)
+  if a_winning_combo?(players_moves)
     puts "Player won!"
     true
-  elsif a_winning_combo?($computer_moves_made)
+  elsif a_winning_combo?(computer_moves)
     puts "Computer won!"
     true
-  elsif the_empty_positions.empty?
+  elsif the_empty_positions(current_game_inputs).empty?
     puts "It's a tie"
     true
   end
 end
 
-def play_tic_tac_toe 
-  display_grid   
+def play_tic_tac_toe(current_game_inputs, players_moves, computer_moves) 
+  display_grid(current_game_inputs)  
   begin
     puts "Choose a position (from 1 to 9) to place your piece"
     player_choice = gets.chomp
-  end while !check_valid_entry(player_choice)
+  end while !check_valid_entry(player_choice,current_game_inputs)
 
-  $game_inputs[player_choice.to_i] = 'x'
-  $player_moves_made << player_choice.to_i
+  current_game_inputs[player_choice.to_i] = 'x'
+  players_moves << player_choice.to_i
 
-  if !a_winning_combo?($player_moves_made) && !the_empty_positions.empty?
-    computer_choice = the_empty_positions.sample
-    $game_inputs[computer_choice] = 'o'
-    $computer_moves_made << computer_choice
+  if !a_winning_combo?(players_moves) && !the_empty_positions(current_game_inputs).empty?
+    computer_choice = the_empty_positions(current_game_inputs).sample
+    current_game_inputs[computer_choice] = 'o'
+    computer_moves << computer_choice
   end
 
   puts `clear`
 
-  display_grid
+  display_grid(current_game_inputs)
 end
 
 WINNING_COMBOS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[3,5,7],[1,5,9]]
-$game_inputs = { 1 => ' ', 2 => ' ', 3 => ' ', 4 => ' ', 5 => ' ', 6 => ' ', 7 => ' ', 8 => ' ', 9 => ' '}
-$player_moves_made = []
-$computer_moves_made =[]
+game_inputs = { 1 => ' ', 2 => ' ', 3 => ' ', 4 => ' ', 5 => ' ', 6 => ' ', 7 => ' ', 8 => ' ', 9 => ' '}
+player_moves_made = []
+computer_moves_made =[]
 
 begin
   puts `clear`
-  play_tic_tac_toe 
-end while !game_over?
+  play_tic_tac_toe(game_inputs, player_moves_made, computer_moves_made) 
+end while !game_over?(game_inputs, player_moves_made, computer_moves_made)
